5950cfcf5ca788372c3d764445c7791d69a48144
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:24:55 -0300

    Update products.js

--- app/backend/Controller/products.js
+++ app/backend/Controller/products.js
@@ -1,33 +1,32 @@
 const route = require('express').Router()
-const {Store,Cart} = require('../models/index')
-const {createWhere,setLimit}  = require('../helpers/index')
-const Cache = require('../cache/index')
+const {Product} = require('../models/index')
+const {generateWhereClause,isValidNumber, sanitizeString}  = require('../helpers/index')
+const limit = 25
 
-const getDatas = async(limit)=>{
-    if(Cache.getCache(`limit=${limit}`))return Cache.getCache(`limit=${limit}`)
-        
-    const datas = await Store.findAll({limit,include: [{ model: Cart, as: 'carts',}]});
-      
-    Cache.setCache(`limit=${limit}`,datas,50000)
-    return datas
-}
 route
-.get('/home/products/:limit?',async(req,res)=>{
-    const limit = setLimit(req.params.limit) 
-    const datas = await getDatas(limit)
+.get('/products',async(req,res)=>{
     try{
+         
+        const datas = await Product.findAll({limit});
         res.status(200).send({message:'sucess',datas})
     }catch(err){
         res.status(500).send({message:'Oops, something went wrong! Please try again later.'+err})
     }
+
 })
 .post('/products/search',async(req,res)=>{
-    const where = createWhere(req.body)
-
+    var {category,name,color,low,high} = req.body
+    
+    if(low && !isValidNumber(low))low = 0
+    if(high && !isValidNumber(high))high = 1000
+    
+    const where = generateWhereClause({
+        low,high,limit,category:sanitizeString(category),
+        name:sanitizeString(name),color:sanitizeString(color)
+    })
     try{
-        const datas = await Store.findAll(where)
-        res.status(200).send({msg:'sucess',datas})
-        console.log(datas)
+       const datas = await Product.findAll(where)
+       res.status(200).send({message:'Sucess',datas})
     }catch(err){
         res.status(500).send({message:'Oops, something went wrong! Please try again later.'+err})
     }
