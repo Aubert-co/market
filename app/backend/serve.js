bab4635ab28d858c096ec00a47515174eb506a41
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:25:43 -0300

    Update serve.js

--- app/backend/serve.js
+++ app/backend/serve.js
@@ -6,20 +6,15 @@ const cors = require("cors")
 require('dotenv').config()
 
 
-/*const session = require('express-session')
-//app.set('trust proxy', 1)
-app.use(session({
-    secret: process.env.SECRET_SESSION,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge:24*60*60*1000 }
-}))*/
-
 app.use(cors())
 app.use(express.json())
+
 app.use('/static',express.static('public'))
+
 app.use(express.urlencoded({extended:true}))
+
 app.use(route)
+
 
 //app.listen(8080,()=>console.log('rodando'))
 
