6b30e415522b593b14dcdc728d317e36cbbca29f
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:24:39 -0300

    Create config.js

---
+++ app/backend/config/config.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+
+const config =  {
+  development: {
+    "username": "root",
+    "password": null,
+    "database": "others2",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging":false,
+    "define": {
+      freezeTableName: true,
+      timestamps: false,
+      plural: false
+    },
+  },
+  test: {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "logging":false,
+    "define": {
+      freezeTableName: true,
+      timestamps: false,
+      plural: false
+    },
+  },
+  production: {
+    "username": "root",
+    "password": null,
+    "database": "others2",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging":false,
+    "define": {
+      freezeTableName: true,
+      timestamps: false,
+      plural: false
+    },
+  }
+}
+
+module.exports = config["test"]
