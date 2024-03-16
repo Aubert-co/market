b0498a642030f403acb46635b11d625ad86e5b8a
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:26:08 -0300

    Update storefixtures.js

--- app/backend/test/storefixtures.js
+++ app/backend/test/storefixtures.js
@@ -1,32 +1,75 @@
 const itemStore = [
-    {id:1,color:'gray',name:"tshirt",img_path:"./tshirts.png",img_name:"tshirt",category:"clothes",price:10},
-    {id:2,color:'orange',name:"pants",img_path:"./pants",img_name:"pants",category:"clothes",price:15},
-    {id:3,color:'red',name:"short",img_path:"./shorts.png",img_name:"shorts",category:"clothes",price:30},
-    {id:4,color:'yellow',name:"nike",img_path:"./dress.png",img_name:"dress",category:"clothes",price:75},
-    {id:5,color:'black',name:"jordan",img_path:"./jordan.png",img_name:"jordan",category:"shoes",price:95},
-    {id:6,color:'black',name:"nike",img_path:"./nike.png",img_name:"nike",category:"shoes",price:85},
-    {id:7,color:'blue',name:"sweater",img_path:"./sweater.png",img_name:"sweater",category:"clothes",price:25},
-    {id:8,color:'green',name:"jacket",img_path:"./jacket.png",img_name:"jacket",category:"clothes",price:50},
-    {id:9,color:'pink',name:"dress",img_path:"./dress.png",img_name:"dress",category:"clothes",price:45},
-    {id:10,color:'white',name:"sneakers",img_path:"./sneakers.png",img_name:"sneakers",category:"shoes",price:80},
-    {id:11,color:'gray',name:"boots",img_path:"./boots.png",img_name:"boots",category:"shoes",price:120},
-    {id:12,color:'red',name:"high heels",img_path:"./heels.png",img_name:"heels",category:"shoes",price:90},
-    {id:13,color:'black',name:"sunglasses",img_path:"./sunglasses.png",img_name:"sunglasses",category:"accessories",price:20},
-    {id:14,color:'brown',name:"watch",img_path:"./watch.png",img_name:"watch",category:"accessories",price:35},
-    {id:15,color:'silver',name:"necklace",img_path:"./necklace.png",img_name:"necklace",category:"accessories",price:15},
-    {id:16,color:'gold',name:"earrings",img_path:"./earrings.png",img_name:"earrings",category:"accessories",price:10},
-    {id:17,color:'purple',name:"backpack",img_path:"./backpack.png",img_name:"backpack",category:"bags",price:40},
-    {id:18,color:'black',name:"wallet",img_path:"./wallet.png",img_name:"wallet",category:"bags",price:25},
-    {id:19,color:'blue',name:"beanie",img_path:"./beanie.png",img_name:"beanie",category:"accessories",price:12},
-    {id:20,color:'gray',name:"gloves",img_path:"./gloves.png",img_name:"gloves",category:"accessories",price:8},
-    {id:21,color:'black',name:"scarf",img_path:"./scarf.png",img_name:"scarf",category:"accessories",price:20},
-    {id:22,color:'white',name:"t-shirt",img_path:"./tshirt-white.png",img_name:"tshirt-white",category:"clothes",price:18},
-    {id:23,color:'green',name:"hoodie",img_path:"./hoodie.png",img_name:"hoodie",category:"clothes",price:45},
-    {id:24,color:'blue',name:"sweatshirt",img_path:"./sweatshirt.png",img_name:"sweatshirt",category:"clothes",price:30},
-    {id:25,color:'brown',name:"boots",img_path:"./boots.png",img_name:"boots",category:"shoes",price:65},
-    {id:26,color:'red',name:"sneakers",img_path:"./sneakers.png",img_name:"sneakers",category:"shoes",price:55}
+    {description:"lorem iptsu",quantity:5,store_id:1,id:1,color:'gray',name:"tshirt",imgPath:"./tshirts.png",img_name:"tshirt",category:"clothes",price:10},
+    {description:"lorem iptsu",quantity:5,store_id:1,id:2,color:'orange',name:"pants",imgPath:"./pants",img_name:"pants",category:"clothes",price:15},
+    {description:"lorem iptsu",quantity:5,store_id:1,id:3,color:'red',name:"short",imgPath:"./shorts.png",img_name:"shorts",category:"clothes",price:30},
+    {description:"lorem iptsu",quantity:5,store_id:1,id:4,color:'yellow',name:"nike",imgPath:"./dress.png",img_name:"dress",category:"clothes",price:75},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:5,color:'black',name:"jordan",imgPath:"./jordan.png",img_name:"jordan",category:"shoes",price:95},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:6,color:'black',name:"nike",imgPath:"./nike.png",img_name:"nike",category:"shoes",price:85},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:7,color:'blue',name:"sweater",imgPath:"./sweater.png",img_name:"sweater",category:"clothes",price:25},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:8,color:'green',name:"jacket",imgPath:"./jacket.png",img_name:"jacket",category:"clothes",price:50},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:9,color:'pink',name:"dress",imgPath:"./dress.png",img_name:"dress",category:"clothes",price:45},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:10,color:'white',name:"sneakers",imgPath:"./sneakers.png",img_name:"sneakers",category:"shoes",price:80},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:11,color:'gray',name:"boots",imgPath:"./boots.png",img_name:"boots",category:"shoes",price:120},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:12,color:'red',name:"high heels",imgPath:"./heels.png",img_name:"heels",category:"shoes",price:90},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:13,color:'black',name:"sunglasses",imgPath:"./sunglasses.png",img_name:"sunglasses",category:"accessories",price:20},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:14,color:'brown',name:"watch",imgPath:"./watch.png",img_name:"watch",category:"accessories",price:35},
+    {description:"lorem iptsu",quantity:5,store_id:2,id:15,color:'silver',name:"necklace",imgPath:"./necklace.png",img_name:"necklace",category:"accessories",price:15},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:16,color:'gold',name:"earrings",imgPath:"./earrings.png",img_name:"earrings",category:"accessories",price:10},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:17,color:'purple',name:"backpack",imgPath:"./backpack.png",img_name:"backpack",category:"bags",price:40},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:18,color:'black',name:"wallet",imgPath:"./wallet.png",img_name:"wallet",category:"bags",price:25},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:19,color:'blue',name:"beanie",imgPath:"./beanie.png",img_name:"beanie",category:"accessories",price:12},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:20,color:'gray',name:"gloves",imgPath:"./gloves.png",img_name:"gloves",category:"accessories",price:8},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:21,color:'black',name:"scarf",imgPath:"./scarf.png",img_name:"scarf",category:"accessories",price:20},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:22,color:'white',name:"t-shirt",imgPath:"./tshirt-white.png",img_name:"tshirt-white",category:"clothes",price:18},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:23,color:'green',name:"hoodie",imgPath:"./hoodie.png",img_name:"hoodie",category:"clothes",price:45},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:24,color:'blue',name:"sweatshirt",imgPath:"./sweatshirt.png",img_name:"sweatshirt",category:"clothes",price:30},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:25,color:'brown',name:"boots",imgPath:"./boots.png",img_name:"boots",category:"shoes",price:65},
+    {description:"lorem iptsu",quantity:5,store_id:3,id:26,color:'red',name:"sneakers",imgPath:"./sneakers.png",img_name:"sneakers",category:"shoes",price:55}
 ]
-
-const defaultLimit = 5
+const stores = [
+    {
+      imgPath: "/path/to/image1.jpg",
+      category: "Electronics",
+      name: "Smartphone",
+      description: "A high-performance smartphone with advanced features.",
+      user_id: 1
+    },
+    {
+      imgPath: "/path/to/image2.jpg",
+      category: "Clothing",
+      name: "T-Shirt",
+      description: "A comfortable cotton t-shirt suitable for everyday wear.",
+      user_id: 2
+    },
+    {
+      imgPath: "/path/to/image3.jpg",
+      category: "Books",
+      name: "Fantasy Novel",
+      description: "An epic fantasy novel filled with magic and adventure.",
+      user_id: 3
+    }
+  ];
+  
+const users = [
+    {
+        id:1,
+      name: "alice",
+      password: "password123"
+    },
+    {
+        id:2,
+      name: "bob",
+      password: "securepass456"
+    },
+    {
+        id:3,
+      name: "charlie",
+      password: "letmein789"
+    }
+  ];
+  
+  
+  
+const defaultLimit = 25
 const maximumLimit = 25
-module.exports = {itemStore,defaultLimit,maximumLimit}
+module.exports = {itemStore,defaultLimit,maximumLimit,stores,users}
