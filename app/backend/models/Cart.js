a93aee3717aeb01d7a4f31512c3179bb193e68af
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:25:31 -0300

    Create Cart.js

---
+++ app/backend/models/Cart.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    static associate(models) {
+      Cart.belongsTo(models.Person, {
+        foreignKey: 'user_id',
+        as: 'person',
+        sourceKey: 'id'
+      });
+      Cart.belongsTo(models.Product, {
+        foreignKey: 'product_id',
+        as: 'product',
+        sourceKey: 'id'
+      });
+    }
+  }
+
+  Cart.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user_id: {
+      allowNull:false,
+      type: DataTypes.INTEGER
+    },
+    size: {
+      type: DataTypes.STRING, 
+      allowNull: true
+    },
+    color: {
+      type: DataTypes.STRING, 
+      allowNull: true 
+    },
+    product_id: {
+      type: DataTypes.INTEGER
+    },
+  }, {
+    sequelize,
+    modelName: 'Cart',
+  });
+
+  return Cart;
+};
