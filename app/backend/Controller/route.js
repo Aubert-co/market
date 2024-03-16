16c07fe2d073546d6a552dc50e47aa78c5f89620
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:24:57 -0300

    Update route.js

--- app/backend/Controller/route.js
+++ app/backend/Controller/route.js
@@ -2,11 +2,12 @@ const route = require('express').Router()
 const cart = require('./UserCart')
 const loginAndRegister = require('./LoginAndRegister')
 const products = require('./products')
-
+const stores = require('./stores')
 
 
 route
 .use(loginAndRegister)
 .use(products)
 .use('/cart',cart)
+.use(stores)
 module.exports = route
