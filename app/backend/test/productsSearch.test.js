77e2476beb48b9b04bce4dd07e4b62db7500f4fd
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:26:03 -0300

    Update productsSearch.test.js

--- app/backend/test/productsSearch.test.js
+++ app/backend/test/productsSearch.test.js
@@ -1,29 +1,39 @@
 const request = require('supertest');
 const server = require('../serve');
-const {Store} = require('../models/index')
+const {Product} = require('../models/index')
 const {Op} = require('sequelize')
 const {itemStore,defaultLimit,maximumLimit} = require('./storefixtures')
 require('dotenv')
 var app
 
 
-describe("GET/products/search",()=>{
+describe("POST/products/search",()=>{
     beforeAll(async()=>{
         try{
             app =  server.listen(8088)
-            await Store.bulkCreate(itemStore)
+            await Product.bulkCreate(itemStore)
         }catch(err){
+            console.log(err)
             throw err
         }
     })
-    
+    it("Should return random items when not sending any category, name, price, color.",async()=>{
+        const response = await request(app)
+        .post('/products/search')
+        .set('Content-Type', 'application/json')
+        
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('datas')
+       
+    })
     it("Should return only items with the category 'shoes' when this category is specified.",async()=>{
         const body = {category:"shoes"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
+ 
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty('datas')
         const {datas} = response.body
@@ -40,37 +50,15 @@ describe("GET/products/search",()=>{
         })
         datas.map((val)=>{expect(val.category).toBe(body.category)})
         
-        expect(datas).toHaveLength(defaultLimit)
     })
     
-    it("Should return random items when not sending any category, name, price, color, or limit.",async()=>{
-        const response = await request(app)
-        .get('/products/search')
-        .set('Content-Type', 'application/json')
-        
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty('datas')
-        const {datas}  =response.body
-        expect(datas).toHaveLength(defaultLimit)
-    })
-    it("Should return only 3 items when sent a limit equal to 3",async()=>{
-        const obj = {limit:3}
-        const response = await request(app)
-        .get('/products/search')
-        .set('Content-Type', 'application/json')
-        .send( obj )
-
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty('datas')
-        const {datas}  =response.body
-
-        expect(datas).toHaveLength(3)
-        expect(datas).not.toHaveLength(defaultLimit)
-    })
+   
+   
+  
     it("Should only return items with the name 'tshirt' and category 'clothes' when these search parameters are sent.",async()=>{
         const body = {category:"clothes",name:"tshirt"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -78,8 +66,8 @@ describe("GET/products/search",()=>{
         expect(response.body).toHaveProperty('datas')
         const {datas} = response.body
         
-        const filterItemStore = itemStore.filter((item)=>item.name ===body.name && item.category === body.category)
-        expect(datas).toHaveLength(filterItemStore.length)
+        const filteritemStore = itemStore.filter((item)=>item.name ===body.name && item.category === body.category)
+        expect(datas).toHaveLength(filteritemStore.length)
         datas.map((val)=>{
             itemStore.filter((item)=>{
                 if(item.id === val.id && item.category === body.category && body.name === item.name){
@@ -103,7 +91,7 @@ describe("GET/products/search",()=>{
     it("Should only return items with a color equal to 'gray'.",async()=>{
         const body = {color:"gray"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -129,7 +117,7 @@ describe("GET/products/search",()=>{
     it("Should only return items with a name equal to 'tshirt'.",async()=>{
         const body = {name:"tshirt"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -152,10 +140,10 @@ describe("GET/products/search",()=>{
         expect(datas.length).toBeLessThanOrEqual(defaultLimit)
        
     })
-    it("If a valid name is sent, but an invalid price is sent, the API should not return any items.",async()=>{
-        const body = {name:"tshirt",price:79}
+    it("If a valid name is sent, but an invalid low price is sent, the API should not return any items.",async()=>{
+        const body = {name:"tshirt",low:1}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -163,16 +151,15 @@ describe("GET/products/search",()=>{
         expect(response.body).toHaveProperty('datas')
         const {datas} = response.body
 
-        const filterItemStore = itemStore.filter((val)=>val.name === body.name && val.price === body.price)
-        
+      
         expect(datas).toHaveLength(0)
-        expect(filterItemStore).toHaveLength(datas.length)
+       
        
     })
     it("Should return nothing when a name that exists, a price that doesn't exist, and a category that exists are sent.",async()=>{
         const body = {name:"tshirt",price:79,category:"shoes"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -181,16 +168,15 @@ describe("GET/products/search",()=>{
 
         const {datas} = response.body
 
-        const filterItemStore = itemStore.filter((val)=>val.name === body.name && val.price === body.price && val.category === body.category)
-        
+     
         expect(datas).toHaveLength(0)
-        expect(filterItemStore).toHaveLength(datas.length)
+       
        
     })
    it("Should return nothing when a name that exists, a price that doesn't exist, a color, and a category that exist are sent.",async()=>{
         const body = {name:"tshirt",price:79,category:"shoes",color:"gray"}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -199,11 +185,9 @@ describe("GET/products/search",()=>{
 
         const {datas} = response.body
         
-        const filterItemStore = itemStore.filter((val)=>val.color === body. color &&val.name === body.name && val.price === body.price && val.category === body.category)
-        
         expect(datas).toHaveLength(0)
-        expect(filterItemStore).toHaveLength(datas.length)
-        expect(filterItemStore).toHaveLength(0)
+      
+      
     })
     
 })
@@ -211,39 +195,12 @@ describe("GET/products/search",()=>{
 
 
 
-describe("GET/products/search testing price",()=>{
-    it("Should only return items with a price equal to 30 if such items exist in the system.",async()=>{
-        const body = {price:30}
-        const response = await request(app)
-        .get('/products/search')
-        .set('Content-Type', 'application/json')
-        .send(body)
-
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty('datas')
-
-        const {datas} = response.body
-      
-
-        datas.map((val)=>{
-            itemStore.filter((item)=>{
-                if(item.id ===val.id){
-                    expect(item.name).toBe(val.name)
-                    expect(item.price).toBe(val.price)
-                    expect(item.category).toBe(val.category)
-                    expect(item.color).toBe(val.color)
-                }
-            })
-            expect(val.price).toBe(body.price)
-        })
-        
-        expect(datas.length).toBeLessThanOrEqual(defaultLimit)
-       
-    })
+describe("POST/products/search testing price",()=>{
+  
     it("Should only return items with a price between 5 and 35.",async()=>{
         const body = {high:35,low:5}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -263,17 +220,16 @@ describe("GET/products/search testing price",()=>{
                 }
             })
             expect(val.price).toBeGreaterThan(body.low)
-            expect(val.price).toBeLessThan(body.high)
+            expect(val.price).toBeLessThanOrEqual(body.high)
             expect(val.price).not.toBeGreaterThan(body.high)
             expect(val.price).not.toBeLessThan(body.low)
         })
-        expect(datas).toHaveLength(defaultLimit)  
-        expect(datas).not.toHaveLength(maximumLimit)
+        expect(datas.length).toBeLessThanOrEqual(25)
     })
     it("Should only return items with a price lower than 45 and a maximum limit of 25 items.",async()=>{
         const body = {low:46,limit:45}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -281,22 +237,18 @@ describe("GET/products/search testing price",()=>{
         expect(response.body).toHaveProperty('datas')
 
         const {datas} = response.body
-        const filterItemStore = itemStore.filter((val)=>val.price < body.low)
-
+       
         datas.map((val)=>{
             expect(val.price).toBeLessThan(body.low)
             expect(val.price).not.toBeGreaterThan(body.low)
         })
-        expect(datas.length).toBeLessThan(maximumLimit)
-        expect(datas.length).not.toBeGreaterThan(maximumLimit)
-        expect(datas.length).not.toBeLessThan(defaultLimit)
-        expect(datas).not.toHaveLength(defaultLimit)
-        expect(datas).toHaveLength(filterItemStore.length)
+       
+        expect(datas.length).toBeLessThanOrEqual(25)
     })
     it("Should only return items with a price greater than 35.",async()=>{
-        const body = {high:35,limit:10}
+        const body = {high:35}
         const response = await request(app)
-        .get('/products/search')
+        .post('/products/search')
         .set('Content-Type', 'application/json')
         .send(body)
 
@@ -314,20 +266,20 @@ describe("GET/products/search testing price",()=>{
                     expect(val.color).toBe(item.color)
                 }
             })
-            expect(val.price).toBeGreaterThan(body.high)
-            expect(val.price).not.toBeLessThan(body.high)
+          
+            expect(val.price).toBeLessThanOrEqual(body.high)
         })
     
-        expect(datas.length).toBeLessThanOrEqual(body.limit)
-        expect(datas.length).not.toBe(maximumLimit)
-        expect(datas.length).not.toBe(defaultLimit)
+         expect(datas.length).toBeLessThanOrEqual(25)
+       
     })
     afterAll(async()=>{
         try{
-           await Store.destroy({ where: { id: { [Op.gt]: 0 } } })
+           await Product.destroy({ where: { id: { [Op.gt]: 0 } } })
         }catch(err){
             console.error(err)
             throw err
         }
     })
+
 })
