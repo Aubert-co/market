46a8d7be568853e2059e4f0cf463c5ae4594c23b
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:25:56 -0300

    Update middleware.test.js

--- app/backend/test/middleware.test.js
+++ app/backend/test/middleware.test.js
@@ -27,53 +27,53 @@ describe("Middleware function.",()=>{
     })
     it("When an invalid authorization is sent, the middleware function Should return a status code of 401 and a message 'Token not provided'.",()=>{
         const {request,response} = headers
-        const func = Middleware(request,response,next)
+        const expectedValue = Middleware(request,response,next)
         
-        expect(func).toHaveProperty("message")
-        expect(func).toHaveProperty("status")
-        expect(func.status).toBe(401)
-        expect(func.message).toBe("Token not provided")
+        expect(expectedValue).toHaveProperty("message")
+        expect(expectedValue).toHaveProperty("status")
+        expect(expectedValue.status).toBe(401)
+        expect(expectedValue.message).toBe("Token not provided")
     })
     it("When an invalid token with only one part is sent, the middleware function Should return status code 401 and a message 'Invalid token format'.",()=>{
         const {request,response} = headers
         request.headers.authorization = token
 
-        const func = Middleware(request,response,next)
+        const expectedValue = Middleware(request,response,next)
 
-        expect(func).toHaveProperty("message")
-        expect(func).toHaveProperty("status")
-        expect(func.status).toBe(401)
-        expect(func.message).toBe("Invalid token format")
+        expect(expectedValue).toHaveProperty("message")
+        expect(expectedValue).toHaveProperty("status")
+        expect(expectedValue.status).toBe(401)
+        expect(expectedValue.message).toBe("Invalid token format")
     })
     it("When a token is sent without the Bearer prefix, the middleware function Should return a 401 status code and the message 'Invalid token format'.",()=>{
         const {request,response} = headers
         request.headers.authorization = token
 
-        const func = Middleware(request,response,next)
+        const expectedValue = Middleware(request,response,next)
 
-        expect(func).toHaveProperty("message")
-        expect(func).toHaveProperty("status")
-        expect(func.status).toBe(401)
-        expect(func.message).toBe("Invalid token format")
+        expect(expectedValue).toHaveProperty("message")
+        expect(expectedValue).toHaveProperty("status")
+        expect(expectedValue.status).toBe(401)
+        expect(expectedValue.message).toBe("Invalid token format")
     })
    
     it("When an invalid token is sent, the middleware function Should return a status code of 401 and a message of 'Invalid token'.",()=>{
         const {request,response} = headers
         request.headers.authorization = `Bearer ${token+"E"}`
 
-        const func = Middleware(request,response,next)
+        const expectedValue = Middleware(request,response,next)
 
-        expect(func).toHaveProperty("message")
-        expect(func).toHaveProperty("status")
-        expect(func.status).toBe(401)
-        expect(func.message).toBe("Invalid token")
+        expect(expectedValue).toHaveProperty("message")
+        expect(expectedValue).toHaveProperty("status")
+        expect(expectedValue.status).toBe(401)
+        expect(expectedValue.message).toBe("Invalid token")
     })
       
     it("When a valid token with the 'Bearer' prefix is provided, the middleware function Should call the 'next()' function and continue to the next middleware in the chain.",()=>{
         const {request,response} = headers
         request.headers.authorization = `Bearer ${token}`
 
-        const func = Middleware(request,response,next)
+        const expectedValue = Middleware(request,response,next)
         
         expect(request.user.user_id).toBe(user_id)
         expect(next).toHaveBeenCalled()
@@ -86,6 +86,8 @@ describe("Routes that use middleware should return the first error in the middle
         app =  server.listen(8090)
        
     })
+   
+   
     it("Route GET/cart/items Should return an error with status code 401 and a message 'Token not provided' when no token is provided.",async()=>{
         const response = await request(app)
         .get(`/cart/items`)
@@ -122,7 +124,63 @@ describe("Routes that use middleware should return the first error in the middle
         expect(response.body.message).toBe('Token not provided')
         expect(response.status).toBe(401)  
     })
+    it("Route GET/store/admin Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .get(`/store/admin`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
+    it("Route POST/store/create Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .post(`/store/create`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
+    it("Route PUT/store/edit Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .put(`/store/edit`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
+    it("Route POST/store/product/create Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .post(`/store/product/create`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
+    it("Route POST/store/product/edit Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .put(`/store/product/edit`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
+    it("Route DELETE/store/product/delete Should return an error with status code 401 and message 'Token not provided' when no token is provided.",async()=>{
+        const response = await request(app)
+        .delete(`/store/product/delete`)
+        expect(response.body.message).not.toBe('sucess')
+        expect(response.status).not.toBe(201)
+      
+        expect(response.body.message).toBe('Token not provided')
+        expect(response.status).toBe(401)  
+    })
 })
+
+
 describe("Routes that do not use middleware should return the first error encountered in the route, without executing subsequent middleware functions or route handlers.",()=>{
     it("route POST/login Should return an error message 'Missing fields' if one or more required fields are not provided.",async()=>{
         const response = await request(app)
@@ -144,21 +202,21 @@ describe("Routes that do not use middleware should return the first error encoun
         expect(response.body.message).not.toBe('Token not provided')
         expect(response.status).not.toBe(401)  
     })
-    it("route GET/home/products  Should return a sucess message",async()=>{
+    it("route GET/products  Should return a sucess message",async()=>{
       
         const response = await request(app)
-        .get(`/home/products/`)
+        .get(`/products/`)
         expect(response.body.message).toBe('sucess')
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty('datas')
         expect(response.body.message).not.toBe('Token not provided')
         expect(response.status).not.toBe(401)  
     })
-    it("route GET/home/search Should return a sucess message",async()=>{
+    it("route POST/products/search Should return a sucess message",async()=>{
     
         const response = await request(app)
-        .get(`/home/products/search`)
-        expect(response.body.message).toBe('sucess')
+        .post(`/products/search`)
+        expect(response.body.message).toBe('Sucess')
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty('datas')
         expect(response.body.message).not.toBe('Token not provided')
