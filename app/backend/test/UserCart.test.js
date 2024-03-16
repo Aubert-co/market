fbd56b754347aa09d42c39d1810ab27e08600a6f
Black <53127242+Aubert-co@users.noreply.github.com>
2024-03-16 17:26:11 -0300

    Update UserCart.test.js

--- app/backend/test/UserCart.test.js
+++ app/backend/test/UserCart.test.js
@@ -1,167 +1,322 @@
 const request = require('supertest');
 const server = require('../serve');
-const {Person,Cart,Store} = require('../models/index')
+const {Person,Cart,Store,Product} = require('../models/index')
 const jwt = require('jsonwebtoken')
 const {Op} = require('sequelize');
-
+const {stores,itemStore,users} = require('./storefixtures')
 require('dotenv').config()
 
 const secret = process.env.SECRET_JWT
-var app,token
+var app,token,cart_ids,quantity_cart
+const [products] = itemStore
+const [user] = users
+const [store]= stores
 
-const personData =  {id:10,name:"lucas",password:'1234567e'}
-const itemStore = {id:5,color:'gray',name:"camisa",img_path:"eqeqe",img_name:"qwjeqe",category:"qwem",price:3}
-const itemCart = {id:3,user_id:personData.id,product_id:itemStore.id,quantity:3}
 
-describe("apis",()=>{
+describe("API CART",()=>{
     beforeAll(async()=>{
         app =  server.listen(8084)
         try{
             await Promise.all([
-                Person.create( personData ),
-                Store.create( itemStore ),
-                Cart.create(  itemCart )
+                Person.create( user ),
+                Store.create( store ),
+                Product.create(products)
             ])
-            token = jwt.sign({user_id:personData.id},secret)
+            token = jwt.sign({user_id:user.id},secret)
         }catch(err){
+         
             console.error("before all"+err)
         }
     })
 
-  
-    describe("GET:cart",()=>{
-        it("should return user's cart items",async()=>{
-            const response = await request(app)
-            .get('/cart/items')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            expect(response.body.message).toBe('sucess')
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty('datas')
-            
-            const {id,quantity,product_id,user_id,store} = response.body.datas[0]
-        
-            expect(id).toEqual(itemCart.id)
-            expect(quantity).toEqual(itemCart.quantity)
-            expect(product_id).toEqual(itemCart.product_id)
-            expect(user_id).toEqual(itemCart.user_id)
-
-            expect(product_id).toEqual(itemStore.id)
-            expect(store.color).toEqual(itemStore.color)
-            expect(store.img_name).toEqual(itemStore.img_name)
-            expect(store.category).toEqual(itemStore.category)
-            expect(store.name).toEqual(itemStore.name)
-            expect(store.img_path).toEqual(itemStore.img_path)
-            expect(store.price).toEqual(itemStore.price)
-    
-        })
-    })
-    describe("PUT:cart/update",()=>{
-        it("when send quantity equal 0 should throw an error and not update the cart",async()=>{
-            const quantityToSend = 0
-            const response =  await request(app)
-            .put('/cart/edit')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({quantity:quantityToSend,cart_id:itemCart.id})
-
-            expect(response.status).toBe(500)
-
-            const {quantity,id} = await Cart.findOne({where:{id:itemCart.id}})
-
-            expect(quantity).toEqual(itemCart.quantity)
-            expect(id).toEqual(itemCart.id)
-        
-        })
-        it("should update quantity in users cart",async()=>{
-            itemCart.quantity = 5
-            const response =  await request(app)
-            .put('/cart/edit')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({quantity:itemCart.quantity,cart_id:itemCart.id})
-
-            expect(response.status).toBe(201)
-
-            const {quantity,id} = await Cart.findOne({where:{id:itemCart.id}})
-
-            expect(quantity).toEqual(itemCart.quantity)
-            expect(id).toEqual(itemCart.id)
-        })
-    })
-    describe("DELETE:cart/delete",()=>{
-        it("when cart id equal a 0 or smaller should return a error",async()=>{
-            const cart_id = 0
-            const response =  await request(app)
-            .delete('/cart/delete')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({quantity:itemCart.quantity,cart_id})
-
-            expect(response.status).toBe(500)
-
-            const datas = await Cart.findOne({where:{id:itemCart.id}})
-
-            expect( datas ).not.toBeNull()
-        })
-        it("should delete item from cart",async()=>{
-            const response =  await request(app)
-            .delete('/cart/delete')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({quantity:itemCart.quantity,cart_id:itemCart.id})
-
-            expect(response.status).toBe(201)
-
-            const datas = await Cart.findOne({where:{id:itemCart.id}})
-
-            expect( datas ).toBeNull()
-        })
-    })
-    describe("CREATE:cart",()=>{
-        it("when send a product id that non existis",async()=>{
-            const response =  await request(app)
-            .post('/cart/create')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({product_id:10})
-
-            expect(response.status).toBe(404)
-
-            const cart = await Cart.findOne({where:{user_id:personData.id}})
-
-            expect(cart).toBeNull()
-        
-        })
-        it("should create a item cart",async()=>{
-            const response =  await request(app)
-            .post('/cart/create')
-            .set('Content-Type', 'application/json')
-            .set('Authorization',`Bearer ${token}`)
-            .send({product_id:itemStore.id})
-
-            expect(response.status).toBe(201)
-
-            const {quantity,product_id,user_id} = await Cart.findOne({where:{user_id:personData.id}})
-
-            expect(quantity).toEqual(1)
-            expect(product_id).toEqual(itemStore.id)
-            expect(user_id).toEqual(personData.id)
-        })
-    })
-  
-   
-   
     afterAll(async()=>{
         try{
             await Promise.all([
-            Person.destroy({ where: { id: { [Op.gt]: 1 } } }),
-            Store.destroy({ where: { id: { [Op.gt]: 1 } } }),
-            Cart.destroy({ where: { user_id: { [Op.gt]: 1 } } })
+                Person.destroy({ where: { id: { [Op.gt]: 0 } } }),
+                Store.destroy({ where: { id: { [Op.gt]: 0 } } }),
+                Product.destroy({ where: { id: { [Op.gt]: 0 } } }),
+                Cart.destroy({where:{id:{[Op.gt]:0}}})
             ])
             app.close()
         }catch(err){
             console.error("afterAll"+err)
         }
     })
+    
+       
+        it("Verifies successful creation of a new cart",async()=>{
+            const response =  await request(app)
+                .post('/cart/create')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({product_id:products.id})
+            
+            
+            expect(response.status).toBe(201)
+            const cart = await Cart.findOne({where:{user_id:user.id},include: { model: Product, as: 'product' }})
+            cart_ids = cart.id
+            const productInDB = await Product.findOne({where:{id:cart.product_id}})
+
+            expect(cart.product_id).toEqual(products.id)
+            expect(cart.quantity).toEqual(1)
+            expect(cart.user_id).toEqual(user.id)
+            
+            expect(productInDB).not.toBeNull()
+
+            quantity_cart = 1
+        })
+        it("When send a cart already exists should update the quantity in +1",async()=>{
+            const response =  await request(app)
+                .post('/cart/create')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({product_id:products.id})
+            
+            
+            expect(response.status).toBe(201)
+            const cart = await Cart.findOne({where:{user_id:user.id},include: { model: Product, as: 'product' }})
+            
+            expect(cart.quantity).toEqual(quantity_cart+=1)
+           
+            
+        })
+        it("Should not create a cart when sending incorrect product IDs",async()=>{
+            const invalidProductId = 'e13'
+            const response =  await request(app)
+                .post('/cart/create')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({product_id:invalidProductId})
+            
+            
+            expect(response.status).toBe(400)
+            const cart = await Cart.findAll({where:{product_id:invalidProductId},include: { model: Product, as: 'product' }})
+
+            expect(cart).toEqual([])
+
+        })
+    
+        it("Should retrieve the items in the user's cart",async()=>{
+            const response = await request(app)
+                .get('/cart/items')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+
+            expect(response.body.message).toBe('sucess')
+            expect(response.status).toBe(200)
+            expect(response.body).toHaveProperty('datas')
+
+            const {dataValues:cart} = await Cart.findOne({where:{user_id:user.id},include: { model: Product, as: 'product' }})
+            
+            const {id,quantity,product_id,user_id,product} = response.body.datas[0]
+            
+            expect(cart.product_id).toBe(product_id)
+            expect(cart.quantity).toEqual(quantity)
+            expect(cart.user_id).toEqual(user_id)
+            expect(cart.id).toEqual(id)
+
+            const productFromDB = await Product.findOne({where:{id:product.id}})
+
+            expect(productFromDB.id).toBe(product.id)
+            expect(productFromDB.store_id).toBe(product.store_id)
+        })
+        
+       
+        it("Should throw an error and not update the cart when quantity is zero",async()=>{
+            const quantityToSend = 0
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:quantityToSend,cart_id:cart_ids,action:'decrease'})
+
+            expect(response.status).toBe(400)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("Should throw an error and refrain from updating the cart when a negative quantity is sent",async()=>{
+            const quantityToSend = -1
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:quantityToSend,cart_id:cart_ids,action:'decrease'})
+
+            expect(response.status).toBe(400)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("When send an invalid action should return an error",async()=>{
+            
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:1,cart_id:cart_ids,action:''})
+
+            expect(response.status).toBe(400)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("When send an invalid action should return an error",async()=>{
+            
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:1,cart_id:cart_ids,action:undefined})
+
+            expect(response.status).toBe(400)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("When send an action different from decrease or increase shound return an error",async()=>{
+            
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:1,cart_id:cart_ids,action:'not'})
+
+            expect(response.status).toBe(400)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("Should update the cart quantity when sending a valid quantity with the 'increase' action",async()=>{
+            const quantityToSend = 5
+            quantity_cart +=5
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:quantityToSend,cart_id:cart_ids,action:'increase'})
+
+            expect(response.status).toBe(201)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("Should update the cart quantity when sending a valid quantity with the 'decrease' action",async()=>{
+            const quantityToSend = 4
+            quantity_cart-=4
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:quantityToSend,cart_id:cart_ids,action:'decrease'})
+
+            expect(response.status).toBe(201)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+
+        })
+        it("when send quantity equal 0 and 'increase' action , should not updated the cart",async()=>{
+       
+            const response =  await request(app)
+            .put('/cart/edit')
+            .set('Content-Type', 'application/json')
+            .set('Authorization',`Bearer ${token}`)
+            .send({quantity:0,cart_id:cart_ids,action:'increase'})
+
+            expect(response.status).toBe(400)
+
+            
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantity_cart)
+        
+        })
+        it("When send a value to 'decrease' greater than the current cart quantity ,the quantity should update to 0 ",async()=>{
+            quantityToSend = 0
+            const response =  await request(app)
+                .put('/cart/edit')
+                .set('Content-Type', 'application/json')
+                .set('Authorization',`Bearer ${token}`)
+                .send({quantity:10,cart_id:cart_ids,action:'decrease'})
+
+            expect(response.status).toBe(201)
+
+            const cart =await  Cart.findByPk(cart_ids)
+            
+            expect(cart.quantity).toEqual(quantityToSend)
+
+        })
+      
+    
+        it("When cart_id equal a 0 should return a error",async()=>{
+            const sendCart_id = 0
+            const response =  await request(app)
+            .delete('/cart/delete')
+            .set('Content-Type', 'application/json')
+            .set('Authorization',`Bearer ${token}`)
+            .send({cart_id:sendCart_id})
+
+            expect(response.status).toBe(400)
+
+            const datas  =await  Cart.findByPk(cart_ids)
+
+            expect( datas ).not.toBeNull()
+        })
+        it("When cart_id equal a negative number should return a error",async()=>{
+            const sendCart_id = -1
+            const response =  await request(app)
+            .delete('/cart/delete')
+            .set('Content-Type', 'application/json')
+            .set('Authorization',`Bearer ${token}`)
+            .send({cart_id:sendCart_id})
+
+            expect(response.status).toBe(400)
+
+            const datas  =await  Cart.findByPk(cart_ids)
+
+            expect( datas ).not.toBeNull()
+        })
+        it("When cart_id equal a NaN number should return a error",async()=>{
+            const sendCart_id = 'eu3y44'
+            const response =  await request(app)
+            .delete('/cart/delete')
+            .set('Content-Type', 'application/json')
+            .set('Authorization',`Bearer ${token}`)
+            .send({cart_id:sendCart_id})
+
+            expect(response.status).toBe(400)
+
+            const datas  =await  Cart.findByPk(cart_ids)
+
+            expect( datas ).not.toBeNull()
+        })
+        
+        
+        it("When send a valid cart_id should delete the cart",async()=>{
+            const response =  await request(app)
+            .delete('/cart/delete')
+            .set('Content-Type', 'application/json')
+            .set('Authorization',`Bearer ${token}`)
+            .send({quantity:products.quantity,cart_id:products.id})
+
+            expect(response.status).toBe(201)
+
+            const datas = await Cart.findOne({where:{id:products.id}})
+
+            expect( datas ).toBeNull()
+        })
 })
