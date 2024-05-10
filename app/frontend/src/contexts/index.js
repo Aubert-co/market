543b946bc7bc81d711978d72bd43e11740f298a3
Black <53127242+Aubert-co@users.noreply.github.com>
2024-05-09 23:52:16 -0300

    Create index.js

---
+++ app/frontend/src/contexts/index.js
@@ -0,0 +1,8 @@
+import React ,{ createContext } from "react";
+
+
+
+export const SearchContext = createContext({name:'',lowPrice:0,highPrice:1000})
+
+
+export const MessageContext = createContext({type:'',content:''})
