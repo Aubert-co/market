d5b5be139ab7bacdb50e253c85aebbaf811c6baa
AUBERT-CO <blacket021@gmail.com>
2024-06-01 23:22:26 -0300

    updated

--- app/backend/Controller/route.js
+++ app/backend/Controller/route.js
@@ -3,11 +3,12 @@ const cart = require('./UserCart')
 const loginAndRegister = require('./LoginAndRegister')
 const products = require('./products')
 const stores = require('./stores')
-
+const tickets = require('./tickets')
 
 route
 .use(loginAndRegister)
 .use(products)
 .use('/cart',cart)
 .use(stores)
+.use(tickets)
 module.exports = route
