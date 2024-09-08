bad70badd56b9c7ee2485a49b55eecf3e9242f4f
Aubert-co <blacket021@gmail.com>
2024-09-07 23:31:05 -0300

    updated

---
+++ app/frontend/src/pages/product.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import { Aside, Aside2, Container,Main ,ProductSection,Header,PromoContainer} from "../style";
+import { TopBar } from "../Components/Header/TopBar";
+import {  SearchContext } from "../Contexts";
+import { BoxMessage } from "../Components/BoxMessage";
+import { BoxItems } from "../Components/BoxItems";
+import { FilterBar } from "../Components/Aside/FilterBar";
+import {  serviceGetItems } from "../services";
+import {items} from '../tests/fixtures';
+import { CartWindow } from "../Components/Aside/CartWindow";
+import { SettingsWindow } from "../Components/Aside/SettingsWindow";
+import { BoxPromotion } from "../Components/BoxPromotion";
+const DEFAULT_SEARCH= {name:'',lowPrice:0,highPrice:1000};
+
+export default function Home(){
+    const [isWindowCart,setIsWindowCart] = useState(false);
+    const [isWindowProfile,setIsWindowProfile] = useState(false);
+    const [searchParams,setSearchParams]= useState( DEFAULT_SEARCH  );
+    const service =async()=>{
+        return {datas:items,status:200};
+    };
+    return (
+        <Container>
+            <SearchContext.Provider value={{searchParams,setSearchParams}}>
+                <Header>
+                    <TopBar isWindowCart={isWindowCart} setIsWindowCart={setIsWindowCart} isWindowProfile={isWindowProfile} setIsWindowProfile={setIsWindowProfile}/>
+               
+                </Header>
+               <Aside>
+                 
+                </Aside>
+          
+                <Main>
+                 
+                </Main>
+
+                <Aside2>
+                    <BoxMessage/>
+                    <CartWindow setIsWindowOpen={setIsWindowCart} isWindowOpen={isWindowCart}/>
+                    <SettingsWindow setIsSettingsOpen={setIsWindowProfile} isSettingsOpen={isWindowProfile}/>
+                </Aside2>
+            </SearchContext.Provider>
+        </Container>
+    )
+};
