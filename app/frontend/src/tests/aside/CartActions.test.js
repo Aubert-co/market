3f9863b48a47726ba407ea1ac9f319855484aa4d
Aubert-co <blacket021@gmail.com>
2024-09-24 23:36:25 -0300

    updated

--- app/frontend/src/tests/aside/CartActions.test.js
+++ app/frontend/src/tests/aside/CartActions.test.js
@@ -41,7 +41,7 @@ describe('Componente CartActions',()=>{
       
        expect(setTottally).toHaveBeenCalledTimes(2)
     })
-    it.only("When decrease , increase and change the value input from the firts element should update only the firts element",()=>{
+    it("When decrease , increase and change the value input from the firts element should update only the firts element",()=>{
         render(
             <CartActions setTottaly={setTottally} id={data[0].id} price={data[0].price} quantity={data[0].quantity}/>
         )
@@ -80,11 +80,11 @@ describe('Componente CartActions',()=>{
         expect(input_quantity.value).toEqual(`${193}`)
         expect(cart_total.textContent).toEqual(`R$${ cartLocal3[0].price * cartLocal3[0].quantity}`)
     })
-    it("When two elements are rendered and change the values from the firts the second must remain the same",()=>{
+    it.only("When two elements are rendered and change the values from the firts the second must remain the same",()=>{
         render(
             <>
-                <CartActions id={data[0].id} price={data[0].price} quantity={data[0].quantity}/>
-                <CartActions id={data[1].id} price={data[1].price} quantity={data[1].quantity}/>
+                <CartActions setTottaly={setTottally} id={data[0].id} price={data[0].price} quantity={data[0].quantity}/>
+                <CartActions setTottaly={setTottally}  id={data[1].id} price={data[1].price} quantity={data[1].quantity}/>
             </>
         )
         const cart_total = screen.getAllByTestId("cart_total")
@@ -98,18 +98,18 @@ describe('Componente CartActions',()=>{
         expect(cart_total[1].textContent).toEqual(`R$${data[1].price* data[1].quantity}`)
         expect(input_quantity[1].value).toEqual(`${data[1].quantity}`)
         fireEvent.click(decrease_btn[0])
+
+        expect(setTottally.mock.calls[0][0]()[0]).toEqual({"id":data[0].id,"total":data[0].price*(data[0].quantity)})
         const cartLocal = JSON.parse( localStorage.getItem('cart') )
 
         mapExpectAll([data[1] ],[cartLocal[1]],'true')
        
-        expect(cartLocal[0].quantity).not.toEqual(data[0].quantity)
-        expect(cartLocal[0].price).toEqual(data[0].price)
-        expect(cartLocal[0].name).toEqual(data[0].name)
-        expect(cartLocal[0].quantity+1).toEqual(data[0].quantity)
+        mapExpectDiferentQnt([cartLocal[0]],[data[0]],data[0].quantity-1)
         expect(input_quantity[1].value).toEqual(`${data[1].quantity}`)
 
        
         fireEvent.click(increase_btn[0])
+        expect(setTottally.mock.calls[0][0]()[0]).toEqual({"id":data[0].id,"total":data[0].price*(data[0].quantity)})
         fireEvent.click(increase_btn[0])
         const cartLocal2 = JSON.parse( localStorage.getItem('cart') )
         mapExpectAll([data[1] ],[cartLocal2[1]],'true')
