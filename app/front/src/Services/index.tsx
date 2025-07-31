ef625bd7cb4195c9732c3c5762251669b148348c
aubert-co <blacket021@gmail.com>
2025-07-30 23:03:22 -0300

    updated

--- app/front/src/Services/index.tsx
+++ app/front/src/Services/index.tsx
@@ -7,19 +7,29 @@ export type Response ={
 }
 
 export const serviceRegister = async(datas:{email:string,name:string,password:string}):Promise<Response >=>{
-
+  
     try{
         const response = await fetch(url+'/register',{
             method:'POST',
             body:JSON.stringify({email:datas.email,name:datas.name,password:datas.password}),
             headers
         })
+       
         if(!response.ok)throw new Error(`${response.status}`);
         const {message} = await response.json()
         return {status:response.status,message} 
     }catch(err:unknown){
-        console.log(err)
-        return {message:'Algo deu errado!',status:500}
+        let status = 500;
+        if (err instanceof Error) {
+            const parsedStatus = Number(err.message);
+            if (!isNaN(parsedStatus)) {
+                status = parsedStatus;
+            }
+        }
+        return {
+            message: "Algo deu errado!",
+            status,
+        };
     }
 }
 export const serviceLogin = async(datas:{email:string,password:string}):Promise<Response >=>{
